578f89c2daf05fffee494d625c28577f927c4d30
jospin6 <jospinndagano1@gmail.com>
2023-10-18 22:30:00 +0200

    Adding meal show page in the app

--- lib/screens/mealDetail.dart
+++ lib/screens/mealDetail.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
+
 class MealDetail extends StatefulWidget {
   final String id;
   const MealDetail({super.key, required this.id});
@@ -16,8 +17,13 @@ class MealDetail extends StatefulWidget {
 
 class _MealDetailState extends State<MealDetail> {
 
+
+
+
+
+
   // late MealDetails meal;
-  late Future<MealDetails> _mealFuture;
+  late Future<Map<String, dynamic>> _mealFuture;
 
   @override
   void initState() {
@@ -25,32 +31,21 @@ class _MealDetailState extends State<MealDetail> {
      _mealFuture = fetchMeal();
   }
 
-   Future<MealDetails> fetchMeal() async {
+Future<Map<String, dynamic>> fetchMeal() async {
     final response = await http.get(Uri.parse('https://www.themealdb.com/api/json/v1/1/lookup.php?i=${widget.id}'));
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final meal = MealDetails.fromJson(data['meals'][0]);
-      return meal;
-    } else {
-      throw Exception('Failed to load meal');
-    }
+    final data = json.decode(response.body);
+    return data['meals'][0];
   }
+   
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-        }, 
-        icon: Icon(Icons.arrow_back_ios)),
-        title: Text("meal.strMeal"),
-      ),
-      body: FutureBuilder<MealDetails>(
+      body: FutureBuilder<Map<String, dynamic>>(
         future: _mealFuture,
         builder: (context, snapshot){
+        
         if (snapshot.hasData) {
           final meal = snapshot.data;
           return SizedBox(
@@ -61,49 +56,79 @@ class _MealDetailState extends State<MealDetail> {
             Container(
               width: MediaQuery.of(context).size.width,
               height: 400,
-              child: Image(image: NetworkImage(meal!.strMealThumb),
+              child: Image(image: NetworkImage(meal?['strMealThumb']),
               fit: BoxFit.cover,
               ),
             ),
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50))
+                ),
+                child: IconButton(onPressed: (){
+                  Navigator.pop(context);
+                }, 
+                icon: const Icon(Icons.arrow_back_ios))
+              )
+              ),
             Positioned(
               top: 350,
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 decoration: const BoxDecoration(
+                  color: Color(0XFFE2B378),
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)
                   )
                 ),
-                child: Column(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Text(meal.strMeal, style: const TextStyle(fontSize: 25),),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.only(
+                          top: 10,
+                          left: 20
+                        ),
+                        child: Text(meal?["strMeal"], style: const TextStyle(fontSize: 25,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(meal?["strInstructions"], style: TextStyle(
+                          fontSize: 16, 
+                        ), textAlign: TextAlign.justify,),
+                      ),
+                      const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        'Ingredients:',
+                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                    const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      'Ingredients:',
-                      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
+                    //  Container(
+                    //   width: MediaQuery.of(context).size.width,
+                    //   child: ListView.builder(
+                    //   scrollDirection: Axis.horizontal,
+                    //   itemCount: 20,
+                    //   itemBuilder: (context, index) {
+                    //     final ingredient = meal?['strIngredient${index + 1}'];
+                    //     final measure = meal?['strMeasure${index + 1}'];
+                    //     return ListTile(
+                    //       leading: CircleAvatar(child: Text('${index + 1}')),
+                    //       title: Text(ingredient),
+                    //       subtitle: Text(measure),
+                    //     );
+                    //   },
+                    // ),
+                    // )
+                    ],
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: meal.ingredients.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(child: Text('${index + 1}')),
-                        title: Text(meal.ingredients[index]),
-                        subtitle: Text(meal.measures[index]),
-                      );
-                    },
-                  ),
-                  )
-                  ],
                 ),
               ))
           ],
@@ -118,32 +143,32 @@ class _MealDetailState extends State<MealDetail> {
 
 
 
-class MealDetails {
-  final String idMeal;
-  final String strMeal;
-  final String strMealThumb;
-  final List<String> ingredients;
-  final List<String> measures;
+// class MealDetails {
+//   final String idMeal;
+//   final String strMeal;
+//   final String strMealThumb;
+//   final List<String> ingredients;
+//   final List<String> measures;
 
-  MealDetails({required this.idMeal, required this.strMeal, required this.strMealThumb, required this.ingredients, required this.measures});
+//   MealDetails({required this.idMeal, required this.strMeal, required this.strMealThumb, required this.ingredients, required this.measures});
 
-  factory MealDetails.fromJson(Map<String, dynamic> json) {
-    List<String> ingredients = [];
-    List<String> measures = [];
+//   factory MealDetails.fromJson(Map<String, dynamic> json) {
+//     List<String> ingredients = [];
+//     List<String> measures = [];
 
-    for (int i = 1; i <= 20; i++) {
-      if (json['strIngredient$i'] != null && json['strIngredient$i'].trim().isNotEmpty) {
-        ingredients.add(json['strIngredient$i']);
-        measures.add(json['strMeasure$i']);
-      }
-    }
+//     for (int i = 1; i <= 20; i++) {
+//       if (json['strIngredient$i'] != null && json['strIngredient$i'].trim().isNotEmpty) {
+//         ingredients.add(json['strIngredient$i']);
+//         measures.add(json['strMeasure$i']);
+//       }
+//     }
 
-    return MealDetails(
-      idMeal: json['idMeal'],
-      strMeal: json['strMeal'],
-      strMealThumb: json['strMealThumb'],
-      ingredients: ingredients,
-      measures: measures,
-    );
-  }
-}
+//     return MealDetails(
+//       idMeal: json['idMeal'],
+//       strMeal: json['strMeal'],
+//       strMealThumb: json['strMealThumb'],
+//       ingredients: ingredients,
+//       measures: measures,
+//     );
+//   }
+// }
